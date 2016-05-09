d12f5258b45478e932aa0e438e8cc3d2affa3cb0
Prashant Sinha <prashant@ducic.ac.in>
2016-05-09 18:49:36 +0530

    + update cname

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/sh
 # Luna Lovegood
 
+rm -rf public
 git branch -f gh-pages
 git checkout gh-pages
 git reset --hard origin/master
 npm run build
 cp -r public/* .
+rm -rf public
 git add -A .
 git commit -a -m 'site update'
 git push origin gh-pages --force
